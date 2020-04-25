907eace7c7d59bce16ab991a145c7616fa15bad7
oscar <oll301189@gmail.com>
2020-04-25 15:51:35 -0300

    tres

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useState,useEffect} from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
 
 
 function App() {
+
+  const [busquedaLetra,guardarBusquedaLetra] = useState({});
+  const [letra,guardarLetra] = useState('');
+
+  useEffect(() => {
+    if(Object.keys(busquedaLetra).length === 0 ) return;
+  
+    const consultarApiLetra = async () => {
+      const {artista,cancion} = busquedaLetra;
+      const url1 =`https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      
+      const resultado = await axios.get(url1);
+      guardarLetra(resultado.date.lyrics);
+    };
+    consultarApiLetra();
+  },[busquedaLetra]);
+
+
   return (
       <Fragment>
-        <Formulario/>
+        <Formulario
+          guardarBusquedaLetra={guardarBusquedaLetra}
+        />
       </Fragment>
-    
-
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const Formulario = () => {
+
+const Formulario = ({guardarBusquedaLetra}) => {
+
+    const [busqueda, guardarBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    });
+   
+    const [ error, guardarError] = useState(false);
+
+    const { artista, cancion } = busqueda;
+
+    // función a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // Consultar las APIS
+    const buscarInformacion = (e) => {
+        e.preventDefault();
+        if(artista.trim() === '' || cancion.trim() === ''){
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+        guardarBusquedaLetra(busqueda);
+    };
+
+
     return ( 
         <div className="bg-info">
+            { error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p> : null}
             <div className="container">
                 <div className="row">
-                    <form
+                    
+                    <form 
+                        onSubmit={buscarInformacion}
                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
                     >
                         <fieldset>
-                            <legend className="text-center">Buscar Letras Canciones
-                            </legend>
+                            <legend className="text-center">Buscador Letras Canciones</legend>
 
                             <div className="row">
                                 <div className="col-md-6">
@@ -19,30 +53,35 @@ const Formulario = () => {
                                         <input
                                             type="text"
                                             className="form-control"
-                                            name-="artista"
+                                            name="artista"
                                             placeholder="Nombre Artista"
+                                            onChange={actualizarState}
+                                            value={artista}
                                         />
                                     </div>
+                                    
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-group">
-                                        <label>Cancion</label>
+                                        <label>Canción</label>
                                         <input
                                             type="text"
                                             className="form-control"
-                                            name-="cancion"
-                                            placeholder="Nombre Cancion"
+                                            name="cancion"
+                                            placeholder="Nombre Canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
                                         />
                                     </div>
                                 </div>
                             </div>
 
-                            <button
-                                type="submit"
+                            <button 
+                                type="submit" 
                                 className="btn btn-primary float-right"
-                            >Buscar
-                            </button>
+                            >Buscar</button>
                         </fieldset>
+
                     </form>
                 </div>
             </div>
